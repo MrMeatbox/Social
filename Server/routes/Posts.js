beff66b19541739aa12d98eb1ef2a89e93430c45
MrMeatbox <86042319+MrMeatbox@users.noreply.github.com>
2021-07-30 21:11:55 +0600

    Add files via upload

---
+++ Server/routes/Posts.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const { Posts, Likes } = require("../models");
+
+const { validateToken } = require("../middlewares/AuthMiddleware");
+
+router.get("/", validateToken, async (req, res) => {
+  const listOfPosts = await Posts.findAll({ include: [Likes] });
+  const likedPosts = await Likes.findAll({ where: { UserId: req.user.id } });
+  res.json({ listOfPosts: listOfPosts, likedPosts: likedPosts });
+});
+
+router.get("/byuserId/:id", async (req, res) => {
+  const id = req.params.id;
+  const listOfPosts = await Posts.findAll({
+    where: { UserId: id },
+    include: [Likes],
+  });
+  res.json(listOfPosts);
+});
+router.post("/", validateToken, async (req, res) => {
+  const post = req.body;
+  post.username = req.user.username;
+  post.UserId = req.user.id;
+  await Posts.create(post);
+  res.json(post);
+});
+
+router.delete("/:postId", validateToken, async (req, res) => {
+  const postId = req.params.postId;
+  await Posts.destroy({
+    where: {
+      id: postId,
+    },
+  });
+
+  res.json("DELETED SUCCESSFULLY");
+});
+
+module.exports = router;
